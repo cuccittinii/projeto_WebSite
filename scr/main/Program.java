7263df6e15ef16adc8186984726940fe2a999a70
Iago Pisa <cuccitini.iago@gmail.com>
2025-08-30 00:21:30 -0300

    Apagando linhas de códigos antigas da classe Program

--- scr/main/Program.java
+++ scr/main/Program.java
@@ -53,49 +53,6 @@ public class Program {
     }
 }
 
-//        System.out.println("-------------------------------------------------------------------");
-//
-//        // --- Cadastro de pets ---
-//        String namePet;  // variável que vai controlar o loop
-//        do {
-//            System.out.print("Pet Name (ou 'sair' para encerrar): ");
-//            namePet = kb.nextLine().trim();
-//
-//            // Se o usuário digitar "sair", encerra o loop
-//            if (namePet.equalsIgnoreCase("sair")) {
-//                break;
-//            }
-//
-//            System.out.print("Specie: ");
-//            String specie = kb.nextLine().trim();
-//
-//            System.out.print("Age: ");
-//            int age = Integer.parseInt(kb.nextLine().trim()); // evita problemas com nextInt + nextLine
-//
-//            // Criando o objeto Pet
-//            Pet pet = new Pet(namePet, specie, age);
-//
-//            // Adicionando o pet à lista do usuário
-//            user.adicionarPet(pet); //!!! NÃO ENTENDI BEM ESSA LINHA, REFAZER E ENTENDER
-//
-//            System.out.println("Pet cadastrado com sucesso!");
-//            System.out.println("-------------------------------------------------------------------");
-//
-//        } while (true); // continua até digitar "sair"
-//
-//        System.out.println("\nCADASTRO REALIZADO COM SUCESSO!");
-//        System.out.println("-------------------------------------------------------------------");
-//
-//        // Exibe informações do usuário
-//        user.exibirInfo();
-//        System.out.println("-------------------------------------------------------------------");
-//
-//        // Exibe todos os pets cadastrados
-//        user.exibirPets();
-//
-//        kb.close();
-//    }
-
 
 //    Services service1 = new Services(Services.Type.Pet_Sitter, 60.0, 60);
 //    Services service2 = new Services(Services.Type.Dog_Walk, 30.0,50);
